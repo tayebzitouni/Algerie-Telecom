387b34ab3d9c43f171e4aef4ad77cbe39e953752
TayebZitouni <tayebzitouni1122111@gmail.com>
2025-12-07 19:06:35 +0100

    group problem4

--- database/migrations/2025_12_02_221128_create_stagiaires_table.php
+++ database/migrations/2025_12_02_221128_create_stagiaires_table.php
@@ -30,7 +30,7 @@ return new class extends Migration
         $table->timestamps();
 
     
-        $table->foreign('group_id')->references('id')->on('groups')->onDelete('set null');
+        $table->foreign('group_id')->references('id')->on('groups_table')->onDelete('set null');
     });
     }
 
