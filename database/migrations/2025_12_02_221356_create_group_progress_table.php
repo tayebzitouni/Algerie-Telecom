c74d2797ecb2ffb93132df797bfdfbedddcb947f
TayebZitouni <tayebzitouni1122111@gmail.com>
2025-12-07 19:10:25 +0100

    group problem 5

--- database/migrations/2025_12_02_221356_create_group_progress_table.php
+++ database/migrations/2025_12_02_221356_create_group_progress_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
         $table->date('date')->nullable();
         $table->timestamps();
 
-        $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+        $table->foreign('group_id')->references('id')->on('groups_table')->onDelete('cascade');
     });
     }
 
