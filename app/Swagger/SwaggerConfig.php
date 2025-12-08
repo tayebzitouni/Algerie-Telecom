affe1ce85581d330de1240dc9012dad370a24340
TayebZitouni <tayebzitouni1122111@gmail.com>
2025-12-08 20:29:34 +0100

    oriin4

--- app/Swagger/SwaggerConfig.php
+++ app/Swagger/SwaggerConfig.php
@@ -4,20 +4,18 @@ namespace App\Swagger;
 
 /**
  * @OA\Info(
- *     version="1.0.0",
- *     title="Stage Hub API",
- *     description="API documentation for Stage Hub",
- *     @OA\Contact(
- *         email="support@example.com"
- *     )
+ *      version="1.0.0",
+ *      title="Stage Hub API",
+ *      description="API documentation for Stage Hub",
+ *      @OA\Contact(email="support@example.com")
  * )
  *
  * @OA\Server(
- *     url=L5_SWAGGER_CONST_HOST,
- *     description="API Server"
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="API Server"
  * )
  */
 class SwaggerConfig
 {
-    // This class is just a container for Swagger annotations
+    // just a container for annotations
 }
