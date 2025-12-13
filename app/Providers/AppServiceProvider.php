81e2e7e18daaf8ccdf5146729fdf82a2220bab04
TayebZitouni <tayebzitouni1122111@gmail.com>
2025-12-13 23:21:46 +0100

    final 6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Mail\MailManager;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
+use App\Mail\Transport\ResendTransport;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,11 +30,6 @@ class AppServiceProvider extends ServiceProvider
             File::makeDirectory($path, 0775, true); // recursive = true
         }
 
-        // Optional: extend MailManager for 'resend' mailer (requires custom transport)
-        $this->app->make(MailManager::class)->extend('resend', function ($app) {
-            // TODO: implement your custom Resend transport here
-            // For now, just throw exception so it's clear it's not implemented
-            throw new \Exception("Custom 'resend' mailer not implemented yet.");
-        });
+      
     }
 }
